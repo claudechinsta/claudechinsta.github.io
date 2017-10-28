ab3a2954dc528161076c90b4c3b1c95be015061a
Boya Claude Chen <claudechinsta@gmail.com>
2017-10-28 16:16:27 +1100

    Renta

--- renta/viewController.js
+++ renta/viewController.js
@@ -1,19 +1,23 @@
 $(document).ready(function () {
-    $('ol').html(localStorage.getItem("content"))
+    // $('ol').html(localStorage.getItem("content"))
+    localStorage.setItem("content", "")
 
     $("#week_rent").on("keyup paste", function () {
         console.log($("#week_rent").val())
         var price = $("#week_rent").val()
-        $("#month_rent").html(price / 7 * 365 / 12 / 2)
-    })
-    $('#button').click(
-        function () {
-            var toAdd = $('input[name=ListItem]').val();
+        $("#month_rent").html( (price / 7 * 365 / 12 / 2).toFixed(2) )
+        $("#month_rent_total").html( (price / 7 * 365 / 12).toFixed(2) )
+    });
+
+    $('#button').click(function () {
+        var toAdd = $('input[name=ListItem]').val();
+        if (toAdd != "") {
             $('ol').append('<li>' + toAdd + '</li>');
             localStorage.setItem("content", $('ol').html())
-        });
+        }
+    });
 
-    $("input[name=ListItem]").keyup(function (event) {
+    $("input[name=ListItem]").keyup(function(event) {
         if (event.keyCode == 13) {
             $("#button").click();
         }
@@ -28,5 +32,4 @@ $(document).ready(function () {
         $(this).val('');
     });
 
-    $('ol').sortable();
 })
