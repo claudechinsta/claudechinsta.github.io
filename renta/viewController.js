64549c9241026f150b9df2dc0aa27193bdaf0265
Boya Claude Chen <claudechinsta@gmail.com>
2017-11-02 09:39:09 +1100

    regular update

--- renta/viewController.js
+++ renta/viewController.js
@@ -1,16 +1,35 @@
 $(document).ready(function () {
     // $('ol').html(localStorage.getItem("content"))
-    localStorage.setItem("content", "") 
+    updateContent() // Initialize the content
     
     function updatePrice(){
-        console.log($("#week_rent").val())
         var price = $("#week_rent").val()
         $("#month_rent").html( (price / 7 * 365 / 12 / 2).toFixed(2) )
         $("#month_rent_total").html( (price / 7 * 365 / 12).toFixed(2) )
     }
-    
+
+    function formatItem(element) {
+        var formatted = "<div class='prop_item' >" +
+        "<div contenteditable='true' class='prop_address single-line'>"+element.address+"</div>" +
+        "<div contenteditable='true' class='prop_price single-line'>$"+element.price+" PW</div>" +
+        "<div contenteditable='true' class='prop_date single-line'>"+element.date+"</div>" +
+        "<div contenteditable='true' class='prop_url'>" +
+        "<a href='"+element.link+"' target='_blank'>"+element.link+"</a></div>" +
+        "<div class='prop_btn'>VISITED</div>" +
+        "</div>"
+        return formatted
+    }
+
+    function updateContent(){
+        $.getJSON("./content.json", function(content){
+            console.log(content)
+            content.forEach(function(element) {
+                $('#prop_list').html(formatItem(element))
+            }, this);
+        })
+    }
+
     $('.prop_item').on('click', '.prop_btn', function(){
-        
         var greyColor = "rgb(180, 180, 180)"
         console.log($(this).parent().css("background-color"))
         if($(this).parent().css("background-color")!=greyColor){
@@ -20,22 +39,19 @@ $(document).ready(function () {
         }
     })
     
-    function updateContent(){
-        localStorage.setItem("content", $('ol').html())
-    }
-    
     $("#week_rent")
     .on("keyup paste", function () {updatePrice()})
     .on("change", function () {updatePrice()});
     
     $('#button').click(function () {
-        $('#prop_list').append("<div class='prop_item' >" +
-        "<div class='prop_address'>405/69 Victoria Street, Fitzroy, Vic 3065</div>" +
-        "<div class='prop_date'>2017-10-29</div>" +
-        "<div class='prop_url'>" +
-        "<a href='https://www.realestate.com.au/rent/in-fitzroy,+vic+3065/list-1' target='_blank'>https://www.realestate.com.au/rent/in-fitzroy,+vic+3065/list-1</a></div>" +
-        "<div class='prop_btn'>VISITED</div>" +
-        "</div>")
+        var d = new Date();
+        var newItem = {
+            "address": "",
+            "date": d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate() ,
+            "price": "",
+            "link": ""
+        }
+        $('#prop_list').append(formatItem(newItem))
     });
     
     $('input').focus(function () {
